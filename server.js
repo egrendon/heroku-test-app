ffcf8385c3f8a4e10647b7824661d1426edb4b04
Ernesto Rendon <egrendon.dev@gmail.com>
2019-08-16 15:08:53 -0700

    FIXES FOR Server.js
    FIXES FOR Server.js

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
+'use strict';
+
+//////OLD
 const express = require('express');
 const app = express();
 const path = require('path');
+const APP_FOLDER_NAME = 'heroku-test-app';
 
 //Use Dist folder
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/dist/${APP_FOLDER_NAME}`));
 
 const portNumber = process.env.PORT || 8080;
 
+// ---- START UP THE NODE SERVER  ----
 app.listen(portNumber, function () {
-    console.log('Node Server.js Listening on port ' + portNumber); //Listening on port 8080
+    console.log('Node Server.js Listening on port http://localhost:' + portNumber); //Listening on port 8080
 });
+
 
 //PathLocationStrategy
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/heroku-test-app/index.html'));
+    let pathForMe = `${__dirname}/dist/${APP_FOLDER_NAME}/index.html`;
+    console.log("get " + pathForMe);
+    res.sendFile(path.join(pathForMe));
 });
 
 
