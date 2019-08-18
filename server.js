3541ab19782661a70b9328b65845f3cb7a27c102
Ernesto Rendon <egrendon.dev@gmail.com>
2019-08-18 08:35:06 -0700

    Comments

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 'use strict';
-
+//Declare express server
 const express = require('express');
 const app = express();
 const path = require('path');
-const APP_FOLDER_NAME = 'heroku-test-app';
+const APP_FOLDER_NAME = 'angular-service-worker-app';
 
-//Use Dist folder
+// Serve only the static files form the dist directory
 app.use(express.static(`${__dirname}/dist/${APP_FOLDER_NAME}`));
 
 const portNumber = process.env.PORT || 8080;
 
-// ---- START UP THE NODE SERVER  ----
+// ---- START UP THE NODE SERVER using ENV or DEFAULT PORT ----
 app.listen(portNumber, function () {
     console.log('Node Server.js Listening on port http://localhost:' + portNumber); //Listening on port 8080
 });
@@ -22,5 +22,3 @@ app.get('/*', function (req, res) {
     console.log("get " + pathForMe);
     res.sendFile(path.join(pathForMe));
 });
-
-
